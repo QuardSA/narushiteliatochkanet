602e20beffa205dee3431925b7bd7897e3ca72d9
QuardSA <smirnov_aleksey04@inbox.ru>
2024-01-22 14:34:32 +0500

    add migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('login');
-            $table->string('fio');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('patronymic');
+            $table->string('login')->unique();
             $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
